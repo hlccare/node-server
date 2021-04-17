39fa135a7acdd00fed518236bd58d387d7002679
hlccare <280108904@qq.com>
2021-04-17 16:41:03 +0800

    update

--- index.ts
+++ index.ts
@@ -1,9 +1,49 @@
 import * as http from 'http'
+import * as fs from 'fs'
+import * as p from 'path'
+import * as url from 'url'
+import { IncomingMessage, ServerResponse } from 'node:http'
+const { URL } = require('url')
 const server = http.createServer()
+const publicDir = p.resolve(__dirname, 'public')
+let cacheAge = 3600 * 24 * 365
 
-server.on('request',(request, response)=>{
-    console.log('有人请求了')
-    response.end('hi')
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    const { method, url: path, headers } = request
+    const { pathname, search } = new URL(path, 'http://localhost:8888')
+
+    if (method !== 'GET') {
+        response.statusCode = 405
+        response.end()
+        return;
+    }
+
+
+
+    let filename = pathname.substring(1)
+    if (filename === '') {
+        filename = 'index.html'
+    }
+    fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+        if (error) {
+            if (error.errno === -4058) {
+                response.statusCode = 404
+                fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+                    response.end(data)
+                })
+            } else if (error.errno === -4068) {
+                response.statusCode = 403
+                response.end('无权查看目录内容')
+            }
+            else {
+                response.statusCode = 500
+                response.end('服务器繁忙，请稍后再试')
+            }
+        } else {
+            response.setHeader('Cache-Control', `public,max-age=${cacheAge}`)
+            response.end(data)
+        }
+    })
 })
 
 server.listen(8888)
